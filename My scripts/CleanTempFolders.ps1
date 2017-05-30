ea171730d99f442a3a5e098dca976fd7be386981
dsinevich <di9383@gmail.com>
2017-05-30 18:06:11 +0300

    Clean-up

--- My scripts/CleanTempFolders.ps1
+++
@@ -1,2 +0,0 @@
-﻿$tempfolders = @( “C:\Windows\Temp\*”, “C:\Windows\Prefetch\*”, “C:\Documents and Settings\*\Local Settings\temp\*”, “C:\Users\*\Appdata\Local\Temp\*” )
-Remove-Item $tempfolders -force -recurse
